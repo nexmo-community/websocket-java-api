f922b6b5f1a289df243c3caf446080621feedf3e
Steve Crow <steve.crow@vonage.com>
2018-10-08 10:58:31 -0400

    Remove unnecessary parameters and throw decelerations.

--- src/main/java/WebSocketEndpoint.java
+++ src/main/java/WebSocketEndpoint.java
@@ -1,19 +1,16 @@
 import javax.websocket.OnMessage;
-import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
-import java.io.IOException;
-import java.nio.ByteBuffer;
 
 @ServerEndpoint("/socket")
 public class WebSocketEndpoint {
     @OnMessage
-    public String handleTextMessage(Session session, String message) throws IOException {
+    public String handleTextMessage(String message) {
         System.out.println("New Text Message Received");
         return message;
     }
 
     @OnMessage(maxMessageSize = 1024000)
-    public byte[] handleBinaryMessage(Session session, byte[] buffer) throws IOException {
+    public byte[] handleBinaryMessage(byte[] buffer) {
         System.out.println("New Binary Message Received");
         return buffer;
     }
